40642aa465e0c263f262d36b7063b450dac9fd7f
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2023-04-07 21:28:25 +0800

    frontend signup

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,56 +1,161 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { AiOutlineCheck } from 'react-icons/ai'
+import { RxCross2 } from 'react-icons/rx'
+
+
+import {
+  matchPasswordChecker,
+  passwordLengthChecker,
+  passwordCharacterChecker,
+  specialSymbolChecker
+} from '../lib/helper/signUpFormChecker'
+
 function SignUp() {
+  const [ password, setPassword ] = useState('')
+  const [ confirmPass, setConfirmPass ] = useState('')
+
+  const [ samePassword, setSamePassword ] = useState(false)
+  const [ digitBool, setDigitBool ] = useState(false)
+  const [ symbolBool, setSymbolBool ] = useState(false)
+  const [ hasNoSpecialSymbolBool, sethasNoSpecialSymbolBool ] = useState(false)
+
 
   function handleSubmit(e) {
     e.preventDefault()
+    console.log(samePassword, digitBool, symbolBool, hasNoSpecialSymbolBool)
+    if ( !samePassword || !digitBool || !symbolBool || !hasNoSpecialSymbolBool) {
+      return
+    }
+
 
     let formData = new FormData(e.currentTarget)
-    formData = Object.fromEntries(formData)
+    formData = Object.fromEntries(formData)    
 
     console.log(formData)
   }
 
+  useEffect(() => {
+
+    matchPasswordChecker(password, confirmPass, setSamePassword)
+    passwordLengthChecker(password, setDigitBool)
+    passwordCharacterChecker(password, setSymbolBool)
+    specialSymbolChecker(password, sethasNoSpecialSymbolBool)
+
+  }, [password, confirmPass])
+
   return (
-    <div>
-        <h1>
+    <div className="mx-auto max-w-[500px]">
+        <h1 className="text-4xl font-bold mt-10 mb-16">
           Create a New Account
         </h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}           
+        >
           <div>
-            <label htmlFor="email">
+            <label htmlFor="email" className="font-semibold text-lg">
               Email
             </label>
-            <input type="email" name="email" required/>
+            <input type="email" name="email" required 
+              className="block mt-2 mb-4 shadow-md text-md px-2 py-1 rounded-md w-[90%] max-w-[400px] focus:bg-blue-200"
+            />
           </div>
           <div>
-            <label htmlFor="firstName">
+            <label htmlFor="firstName" className="font-semibold text-lg">
               First Name
             </label>
-            <input type="text" name="firstName" required/>
+            <input type="text" name="firstName" required 
+              className="block mt-2 mb-4 shadow-md text-md px-2 py-1 rounded-md w-[90%] max-w-[400px] focus:bg-blue-200"
+            />
           </div>
           <div>
-            <label htmlFor="lastName">
+            <label htmlFor="lastName" className="font-semibold text-lg">
               Last Name
             </label>
-            <input type="text" name="lastName" required/>
+            <input type="text" name="lastName" required 
+              className="block mt-2 mb-4 shadow-md text-md px-2 py-1 rounded-md w-[90%] max-w-[400px] focus:bg-blue-200"
+            />
           </div>
-          <div>
-            <label htmlFor="password">
+          <div className="relative">
+            <label htmlFor="password" className="font-semibold text-lg">
               Password
             </label>
-            <input type="password" name="password" required/>
+            <input 
+              className="block mt-2 mb-4 shadow-md text-md px-2 py-1 rounded-md w-[90%] max-w-[400px] focus:bg-blue-200 relative"
+              type="password" 
+              name="password" 
+              required
+              onChange={(e) => setPassword(e.currentTarget.value)}          
+            />
           </div>
-          <div>
-            <label htmlFor="confirmPass">
+          <div className="relative">
+            <label htmlFor="confirmPass" className="font-semibold text-lg">
               Confirm Password
             </label>
-            <input type="password" name="confirmPass" required/>
+            <input 
+              className="block mt-2 mb-4 shadow-md text-md px-2 py-1 rounded-md w-[90%] max-w-[400px] focus:bg-blue-200"
+              type="password" 
+              name="confirmPass" 
+              required
+              onChange={(e) => setConfirmPass(e.currentTarget.value)}
+      
+            />
           </div>
+            {
+            samePassword ?
+            <div className="text-green-600">
+              <AiOutlineCheck className="inline me-4"/>
+              Pasword Matched
+            </div> :
+            <div 
+              className="text-red-600"
+            >
+              <RxCross2 className="inline me-4"/>
+              Password Don't Matched
+            </div>
+          }
+          {
+            digitBool ?
+            <div className="text-green-600">
+              <AiOutlineCheck className="inline me-4"/>
+              Pasword Must Be At Least 6 Characters
+            </div> :
+            <div 
+              className="text-red-600"
+            >
+              <RxCross2 className="inline me-4"/>
+              Pasword Must Be At Least 6 Characters
+              
+            </div>
+          }
+          {
+            symbolBool ?
+            <div className="text-green-600">
+              <AiOutlineCheck className="inline me-4"/>
+              Pasword Must Contain Characters A-Z a-z 0-9
+            </div> :
+            <div 
+              className="text-red-600"
+            >
+              <RxCross2 className="inline me-4"/>
+              Pasword Must Contain Characters A-Z a-z 0-9              
+            </div>
+          }
+          {
+            !hasNoSpecialSymbolBool &&
+            <div 
+              className="text-red-600"
+            >
+              <RxCross2 className="inline me-4"/>
+              Pasword Must Not Contain a Special Symbol or Whitespace
+            </div>
+          }
           <div>
-            <input type="submit" value='create' />
+            <input type="submit" value='Submit'
+              className="px-2 py-1 bg-green-300 rounded-lg drop-shadow-md hover:scale-110 mt-4"
+            />
           </div>
         </form>
-      <form>
-      </form>
+        
     </div>
   )
 }
