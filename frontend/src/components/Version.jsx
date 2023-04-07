799198943921a1ade417783819c553b6e2fb4427
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2023-04-07 13:00:45 +0800

    fixed

--- frontend/src/components/Version.jsx
+++ frontend/src/components/Version.jsx
@@ -1,6 +1,6 @@
 function Version() {
   return (
-    <div className="absolute bottom-4 right-4 opacity-70 italic text-sm">
+    <div className="fixed bottom-4 right-4 opacity-70 italic text-sm">
       version 0.0.01
     </div>
   )
